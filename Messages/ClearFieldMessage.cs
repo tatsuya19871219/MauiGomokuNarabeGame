9858196eaf5b04f07f5c18a3e44874a397b9a046
Tatsuya Yamada <tatsuya19871219@gmail.com>
2023-05-13 17:19:58 +0900

    Add Message classes

---
+++ Messages/ClearFieldMessage.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace MauiGomokuNarabeGame.Messages;
+
+public class ClearFieldMessage : ValueChangedMessage<string>
+{
+    public ClearFieldMessage(string value) : base(value)
+    {
+    }
+}
---
+++ Messages/FillPoolMessage.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace MauiGomokuNarabeGame.Messages;
+
+public class FillPoolMessage : ValueChangedMessage<string>
+{
+    public FillPoolMessage(string value) : base(value)
+    {
+    }
+}
