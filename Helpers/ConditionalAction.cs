e1b3cd534ce86870362ae3bb08b34cb94c6a3d4a
Tatsuya Yamada <tatsuya19871219@gmail.com>
2023-05-14 10:45:59 +0900

    Update ConditionalAction class

--- Helpers/ConditionalAction.cs
+++ Helpers/ConditionalAction.cs
@@ -2,25 +2,38 @@
 
 internal class ConditionalAction
 {
-    required public Action ReservedAction { get; init; }
+    public int MillisecondsDelay { get; init; } = 100;
+    public int MillisecondsTimeout { get; init; } = 1000;
 
-    public async void InvokeIf(Func<bool> condition, int millisecondsDelay = 100, int millisecondsTimeout = 1000)
+    readonly Action _action;
+    readonly Func<bool> _condition;
+
+    public ConditionalAction(Action action, params Func<bool>[] conditions)
     {
-        if (condition is null) throw new ArgumentNullException(nameof(condition));
-        if (millisecondsDelay <= 0) throw new ArgumentOutOfRangeException(nameof(millisecondsDelay));
-        if (millisecondsTimeout <= 0) throw new ArgumentOutOfRangeException(nameof(millisecondsTimeout));
+        if (action is null) throw new ArgumentNullException(nameof(action));
+        if (conditions is null) throw new ArgumentNullException(nameof(conditions));
+
+        _action = action;
+        _condition = () => conditions.All(c => c.Invoke());
+    }
+
+    public async void Invoke()
+    {
+        if (MillisecondsDelay <= 0) throw new ArgumentOutOfRangeException(nameof(MillisecondsDelay));
+        if (MillisecondsTimeout <= 0) throw new ArgumentOutOfRangeException(nameof(MillisecondsTimeout));
 
         int elapsedDelay = 0;
         while (true)
         {
-            if (elapsedDelay > millisecondsTimeout) throw new TimeoutException("Reserved action is never executed");
+            if (elapsedDelay > MillisecondsTimeout) throw new TimeoutException("Conditional action faces timeout.");
 
-            if (condition.Invoke()) break;
+            if (_condition.Invoke()) break;
 
-            elapsedDelay += millisecondsDelay;
-            await Task.Delay(millisecondsDelay);
+            elapsedDelay += MillisecondsDelay;
+            await Task.Delay(MillisecondsDelay);
         }
 
-        ReservedAction.Invoke();
+        _action.Invoke();
     }
+
 }
